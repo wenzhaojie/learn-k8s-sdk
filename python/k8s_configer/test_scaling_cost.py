8e6e85797c714312e2f4f9886951d48593881a71
zhaojie <8432624+wen-zhaojie@user.noreply.gitee.com>
2022-12-04 17:11:55 +0800

    Update for exp_7()

--- python/k8s_configer/test_scaling_cost.py
+++ python/k8s_configer/test_scaling_cost.py
@@ -85,6 +85,8 @@ def exp_different_replicas_resource_node(name, namespace, init_replicas_list, ta
                 for (init_replicas, target_replicas) in itertools.product(init_replicas_list, target_replicas_list):
                     scaling_t = scaling_deployment(name=name, namespace=namespace, init_replicas=init_replicas, target_replicas=target_replicas, is_wait_until_no_unavailable=is_wait_until_no_unavailable)
                     log_dict = {
+                        "name": name,
+                        "namespace": namespace,
                         "node_name_list": node_name_list,
                         "node_num": len(node_name_list),
                         "init_replicas": init_replicas,
@@ -123,8 +125,67 @@ def exp_different_replicas_resource_node(name, namespace, init_replicas_list, ta
             print(f"保存完成至{filepath}!")
 
 
+def exp_different_replicas_resource_node_with_tight_repeat(name, namespace, init_replicas_list, target_replicas_list, resource_list,
+                                         node_name_list_list, is_wait_until_no_unavailable=False,
+                                         filename="scaling-cost.csv", save_root="./results", repeat=1):
+    datetime_str = strftime("%Y-%m-%d-%H-%M-%S", localtime())
+    filename = datetime_str + "-" + filename
+    filepath = os.path.join(save_root, filename)
+
+    log_dict_list = []
+
+    for node_name_list in node_name_list_list:
+        my_configer.upgrade_deployment_affinity(name=name, namespace=namespace, node_name_list=node_name_list)
+        for resource in resource_list:
+            my_configer.update_deployment_resource(name=name, namespace=namespace, resource=resource)
+            for (init_replicas, target_replicas) in itertools.product(init_replicas_list, target_replicas_list):
+                for iter in range(repeat):
+                    print(f"第{iter}遍!")
+                    scaling_t = scaling_deployment(name=name, namespace=namespace, init_replicas=init_replicas,
+                                                   target_replicas=target_replicas,
+                                                   is_wait_until_no_unavailable=is_wait_until_no_unavailable)
+                    log_dict = {
+                        "name": name,
+                        "namespace": namespace,
+                        "node_name_list": node_name_list,
+                        "node_num": len(node_name_list),
+                        "init_replicas": init_replicas,
+                        "target_replicas": target_replicas,
+                        "delta_replicas": target_replicas - init_replicas,
+                        "scaling_t": scaling_t,
+                        "is_wait_until_no_unavailable": is_wait_until_no_unavailable
+                    }
+                    log_dict.update(resource)
+                    # 写入txt
+                    with open(filepath + ".txt", mode='a') as f:
+                        f.write(str(log_dict) + "\n")
+
+                    print(f"log_dict:{log_dict}")
+                    log_dict_list.append(log_dict)
+
+                    # 将pod清零
+                    my_configer.upgrade_deployment_replicas(name=name, namespace=namespace, replicas=0)
+                    wait_t = wait_deployment_replicas_until_no_unavailable(name=name, namespace=namespace, timeout=30)
+                    # 等待 terminating
+                    if math.isnan(scaling_t):
+                        time.sleep(10)
+                    else:
+                        time.sleep(scaling_t + 2)
+
+                    if math.isnan(wait_t):
+                        print(f"清零失败!")
+                    else:
+                        print(f"清零成功!")
+
+        utils.write_dict_list_to_csv(
+            filepath=filepath,
+            log_dict_list=log_dict_list
+        )
+        print(f"保存完成至{filepath}!")
+
+
 def do_exp():
-    name = "app"
+    name = "nginx"
     namespace = "default"
     init_replicas_list = [0]
     target_replicas_list = [i for i in range(0,17)]
@@ -152,7 +213,7 @@ def do_exp():
 
 
 def do_exp_2():
-    name = "app"
+    name = "nginx"
     namespace = "default"
     init_replicas_list = [0]
     target_replicas_list = [i for i in range(0,65)]
@@ -181,7 +242,7 @@ def do_exp_2():
 
 
 def do_exp_3():
-    name = "app"
+    name = "nginx"
     namespace = "default"
     init_replicas_list = [0]
     target_replicas_list = [i for i in range(0,65)]
@@ -209,7 +270,7 @@ def do_exp_3():
     )
 
 def do_exp_4():
-    name = "app"
+    name = "nginx"
     namespace = "default"
     init_replicas_list = [0]
     target_replicas_list = [i for i in range(0,21)]
@@ -237,7 +298,7 @@ def do_exp_4():
     )
 
 def do_exp_5():
-    name = "app"
+    name = "nginx"
     namespace = "default"
     init_replicas_list = [0]
     target_replicas_list = [i for i in range(1,31)]
@@ -266,7 +327,7 @@ def do_exp_5():
 
 
 def do_exp_6():
-    name = "app"
+    name = "nginx"
     namespace = "default"
     init_replicas_list = [0]
     target_replicas_list = [i for i in range(1,31)]
@@ -294,7 +355,36 @@ def do_exp_6():
     )
 
 
+def do_exp_7():
+    name = "busybox"
+    namespace = "default"
+    init_replicas_list = [0]
+    target_replicas_list = [i for i in range(1,31)]
+    resource_list = utils.generate_resource_list(cpu_list=[0.1], memory_list=[64])
+    node_name_list_list = [
+        ["k8s02"],
+        ["k8s02", "k8s03"],
+        ["k8s02", "k8s03","k8s04"],
+        # ["k8s02", "k8s03", "k8s04", "k8s05"],
+        # ["k8s02", "k8s03", "k8s04", "k8s05", "k8s06"],
+        ["k8s02", "k8s03", "k8s04", "k8s05", "k8s06", "k8s07"],
+        # ["k8s02", "k8s03", "k8s04", "k8s05", "k8s06", "k8s07", "k8s08"],
+    ]
+    is_wait_until_no_unavailable = False
+
+    exp_different_replicas_resource_node_with_tight_repeat(
+        name=name,
+        namespace=namespace,
+        init_replicas_list=init_replicas_list,
+        target_replicas_list=target_replicas_list,
+        resource_list=resource_list,
+        node_name_list_list=node_name_list_list,
+        is_wait_until_no_unavailable=is_wait_until_no_unavailable,
+        repeat=20
+    )
+
+
 
 if __name__ == "__main__":
     # print(scaling_deployment(name="app", namespace="default", init_replicas=0, target_replicas=5))
-    do_exp_6()
+    do_exp_7()
